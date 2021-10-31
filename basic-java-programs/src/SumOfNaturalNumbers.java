ca52988ec48b154ecb5bbe6abb61872290e802cd
anupam-pratihar <anupampratihar10@gmail.com>
2021-10-31 11:15:26 +0530

    Added the 4th program.

--- basic-java-programs/src/SumOfNaturalNumbers.java
+++ basic-java-programs/src/SumOfNaturalNumbers.java
@@ -1,8 +1,21 @@
+/*
+ * Natural numbers
+
+Natural number are those number  which is start from one to infinite.
+
+A natural number is a number that occurs commonly and obviously in nature. As such, it is a whole, 
+non-negative number.
+
+Example: Natural number={1,2,,4,5,6,…….}.
+
+So we will perform the calculation Operation on Natural number using C++ programming approach.
+ */
+
 
 public class SumOfNaturalNumbers {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
